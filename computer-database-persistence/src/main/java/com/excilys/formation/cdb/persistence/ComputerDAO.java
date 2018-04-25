97b96574c57103ca8f7d7be7d11dad4d853372bf
pnexer <pnexer@excilys.com>
2018-04-25 16:10:10 +0200

    [refacto] maven_muli_modul

---
+++ computer-database-persistence/src/main/java/com/excilys/formation/cdb/persistence/ComputerDAO.java
@@ -0,0 +1,109 @@
+package com.excilys.formation.cdb.persistence;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.excilys.formation.cdb.mapper.CompanyMapper;
+import com.excilys.formation.cdb.mapper.ComputerMapper;
+import com.excilys.formation.cdb.model.Computer;
+
+@Repository
+public class ComputerDAO {
+
+	
+	private ComputerMapper computerMapper;
+	private JdbcTemplate jdbcTemplate;
+	
+	 @Autowired
+	 public ComputerDAO(DataSource dataSource, ComputerMapper computerMapper) {
+	      this.jdbcTemplate = new JdbcTemplate(dataSource);
+	      this.computerMapper = computerMapper;
+	 }
+   
+    private  String selectListRequest = "SELECT cu.id as cuId, cu.name as cuName, introduced, discontinued, ca.id as caId, ca.name as caName FROM computer cu LEFT JOIN company ca ON ca.id = cu.company_id";
+    private  String createRequest = "INSERT INTO computer (name, introduced, discontinued, company_id) VALUES (?, ?, ?, ?);";
+    private  String updateRequest = "UPDATE computer SET name = ?, introduced = ?, discontinued = ?, company_id = ? WHERE id = ?;";
+    private  String deleteRequest = "DELETE FROM computer WHERE id=?;";
+    private  String countRequest = "SELECT count(id) FROM computer;";
+    private  String selectComputer= "SELECT cu.id as cuId, cu.name as cuName, introduced, discontinued, ca.id as caId, ca.name as caName FROM computer cu LEFT JOIN company ca ON ca.id = cu.company_id WHERE cu.id = ?;";
+
+
+    public int countAllComputer() {  
+    	int res = this.jdbcTemplate.queryForObject(countRequest, Integer.class);
+         return res;
+    }
+
+    public List<Computer> listComputer() {
+    	List<Computer> res =this.jdbcTemplate.queryForList(selectListRequest, Computer.class);
+    	return res;
+    }
+
+    public void createComputer(Computer computer) {
+    	  this.jdbcTemplate.update(createRequest, preparedStatement -> {computerToPreparedStatement(computer, preparedStatement);});
+    	
+    }
+
+    public void updateComputer(Computer computer) {
+     	  this.jdbcTemplate.update(updateRequest, preparedStatement -> {computerToPreparedStatement(computer, preparedStatement);});
+
+    }
+
+    public void deleteComputer(int id) {
+    	this.jdbcTemplate.update(deleteRequest, id);
+    }
+
+    public Optional<Computer> selectComputer(int id) {
+    	  Computer computer;         
+          computer = this.jdbcTemplate.query(selectComputer, (ResultSet resultSet, int row) -> computerMapper.resultSetToComputer(resultSet), id).get(0);     
+          return Optional.ofNullable(computer);
+    }
+     
+    public List<Computer> subList(int offset, int limit,String keyword) {
+    	   String like = "";
+           Object[] object;
+ 	
+    	 if (keyword.length() > 0) {
+             object = new Object[] {"%" + keyword + "%", limit, offset};
+             like = " WHERE cu.name LIKE ?";
+         } else {
+             object = new Object[] {limit, offset};
+         }
+    	 
+         String request = selectListRequest  + like + " LIMIT ? OFFSET ?;";
+         return this.jdbcTemplate.query(request, object, (ResultSet resultSet, int row) -> computerMapper.resultSetToComputer(resultSet));
+    }
+    
+    //Jirr's method
+    public PreparedStatement computerToPreparedStatement(Computer computer, PreparedStatement preparedStatement) throws SQLException {
+        preparedStatement.setString(1, computer.getName());
+        if (computer.getDateIntroduced().isPresent()) {
+            preparedStatement.setDate(2, Date.valueOf(computer.getDateIntroduced().get()));
+        } else {
+            preparedStatement.setNull(2, java.sql.Types.DATE);
+        }
+        if (computer.getDateDiscontinued().isPresent()) {
+            preparedStatement.setDate(3, Date.valueOf(computer.getDateDiscontinued().get()));
+        } else {
+            preparedStatement.setNull(3, java.sql.Types.DATE);
+        }
+        if (computer.getManufactor().isPresent()) {
+            preparedStatement.setInt(4, computer.getManufactor().get().getId());
+        } else {
+            preparedStatement.setNull(4, java.sql.Types.INTEGER);
+        }
+        return preparedStatement;
+    }
+}
---
+++ computer-database-webapp/src/main/java/com/excilys/formation/cdb/controller/GlobalController.java
@@ -0,0 +1,97 @@
+ package com.excilys.formation.cdb.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.excilys.formation.cdb.dto.ComputerDTO;
+import com.excilys.formation.cdb.mapper.ComputerMapper;
+import com.excilys.formation.cdb.service.CompanyService;
+import com.excilys.formation.cdb.service.ComputerService;
+import com.excilys.formation.cdb.service.ComputerPage;
+
+@Controller
+public class GlobalController {
+	
+	private ComputerService computerService;
+    private CompanyService companyService;
+    private ComputerMapper computerMapper;
+
+    public GlobalController(ComputerService computerService, CompanyService companyService, ComputerMapper computerMapper) {
+        this.computerService = computerService;
+        this.companyService = companyService;
+        this.computerMapper = computerMapper;
+    }
+    
+    @GetMapping("/dashboard")
+    public ModelAndView doGetDashboard(@RequestParam(value = "search", defaultValue = "") String search,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "index", defaultValue = "1") int index){
+       
+        ModelAndView modelAndView = new ModelAndView();
+        ComputerPage page = new ComputerPage(size,computerService);
+        
+        int nbComputer = computerService.countComputers();
+        List<ComputerDTO> computerDTOList = new ArrayList<>();
+        page.getContent().forEach(computer -> computerDTOList.add(computerMapper.computerToComputerDTO(computer)));
+        modelAndView.addObject("nbComputers", nbComputer);
+        modelAndView.addObject("computerlist", computerDTOList);
+        modelAndView.addObject("size", page.getSize());
+        modelAndView.addObject("keywords", page.getKeywords());
+        modelAndView.addObject("maxIndex", page.getLastPageIndex() + 1);
+        modelAndView.addObject("currentIndex", page.getCurrentPageIndex() + 1);
+        return modelAndView;
+    }
+
+    @PostMapping("/dashboard")
+    public ModelAndView doPostDashboard(@RequestParam(value = "selection") int id) {
+        ModelAndView modelAndView = new ModelAndView();
+        return modelAndView;
+    }
+    
+    @GetMapping("/add")
+    public ModelAndView doGetAddComputer() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("companyList", companyService.selectListCompany());
+        modelAndView.addObject("computer", new ComputerDTO());
+        return modelAndView;
+    }
+
+    @PostMapping("/add")
+    public ModelAndView doPostAddComputer(@ModelAttribute("computer") ComputerDTO computerDTO, Model model) {
+        ModelAndView modelAndView = new ModelAndView();   
+        return modelAndView;
+    }
+    
+    @GetMapping("/edit")
+    public ModelAndView doGetEditComputer(@RequestParam(value = "id") int id) {
+    	 
+    	ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("companyList", companyService.selectListCompany());
+        ComputerDTO computerDTO = null;
+		try {
+			computerDTO = computerMapper.computerToComputerDTO(computerService.selectOneComputer(id));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        modelAndView.addObject("computer", computerDTO);
+      
+        return modelAndView;
+    }
+
+    @PostMapping("/edit")
+    public ModelAndView doPostEditComputer(@ModelAttribute("computer") ComputerDTO computerDTO, Model model) {
+        ModelAndView modelAndView = new ModelAndView();
+   
+        return modelAndView;
+    }
+    
+
+}
